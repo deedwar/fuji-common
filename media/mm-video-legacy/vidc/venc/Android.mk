8ceadd50ebedc401883150943505751db6c44e86
bryan2894 <bryan2894@gmail.com>
2014-04-11 13:03:15 -0400

    add required files

--- media/mm-video-legacy/vidc/venc/Android.mk
+++ media/mm-video-legacy/vidc/venc/Android.mk
@@ -46,8 +46,8 @@ libmm-venc-inc      := bionic/libc/include
 libmm-venc-inc      += bionic/libstdc++/include
 libmm-venc-inc      := $(LOCAL_PATH)/inc
 libmm-venc-inc      += $(OMX_VIDEO_PATH)/vidc/common/inc
-libmm-venc-inc      += hardware/qcom/media/mm-core/inc
-libmm-venc-inc      += hardware/qcom/media/libstagefrighthw
+libmm-venc-inc      += device/sony/fuji-common/media/mm-core/inc
+libmm-venc-inc      += device/sony/fuji-common/media/libstagefrighthw
 
 libmm-venc-inc      += device/sony/fuji-common/display/libgralloc
 

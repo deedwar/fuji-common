06e5f9149a31187341410f4701b4c51c7872c8ed
Warren <deed821@gmail.com>
2014-05-18 02:55:33 -0700

    random fixing
    Change-Id: I4f913fd3c88ab5498e1035e0d7a881a754cfe087

--- power/Android.mk
+++ power/Android.mk
@@ -6,7 +6,7 @@ ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
 # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
 LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c
 LOCAL_MODULE := power.fuji
